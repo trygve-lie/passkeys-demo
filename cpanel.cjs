065a92af938e8f3ad2b148f57bf84c075d84e44d
Trygve Lie <post@trygve-lie.com>
2025-03-13 19:59:37 +0100

    Added cpanel comment

--- cpanel.cjs
+++ cpanel.cjs
@@ -1,3 +1,13 @@
+// Cpanel does not cater well for ESM projects in the way
+// that it does not keep package.json with it so we loose
+// the config that the project is a ESM project.
+//
+// Cpanel does more or less only understand CommonJS modules.
+//
+// This little file is a CommonJS module with a small trick
+// which loads ESM modules into a CommonJS module (which cpane
+// understands).
+
 async function loadApp() {
     await import('./server.js');
 }
